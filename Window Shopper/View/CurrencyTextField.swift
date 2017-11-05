c53b0047b453d3a4f9588b8de479c9f099a4c57b
Jefffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-05 17:38:18 -0500

    Logic complete. Added locale currency into textfields

--- Window Shopper/View/CurrencyTextField.swift
+++ Window Shopper/View/CurrencyTextField.swift
@@ -11,6 +11,23 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7659620876, green: 0.7659620876, blue: 0.7659620876, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +41,7 @@ class CurrencyTextField: UITextField {
         
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
+        clipsToBounds = true
         textAlignment = .center
         
         if let ph = placeholder {
