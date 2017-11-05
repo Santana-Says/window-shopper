02f74af0b6be3fa83065409ab7af965c66aa7564
Jefffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-05 16:52:02 -0500

    UI is complete. Created calculate button programatically

---
+++ Window Shopper/Controller/MainVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Jefffrey Santana on 11/5/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+    
+    @objc func calculate() {
+        print("Calc pressed")
+    }
+
+}
+
---
+++ Window Shopper/View/CurrencyTextField.swift
@@ -0,0 +1,36 @@
+//
+//  CurrencyTextField.swift
+//  Window Shopper
+//
+//  Created by Jefffrey Santana on 11/5/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let ph = placeholder {
+            let place = NSAttributedString(string: ph, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+
+}
